a2041e0b7bce9ac0ae44a60d5cf2e6f1e739f8b2
Chris <liqdfire@gmail.com>
2021-03-25 16:31:02 -0400

    Init commit update readme
    fix BepInEx link in readme

---
+++ src/BepInExMod.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace ValheimSherpentHunter
+{
+  [BepInPlugin("org.bepinex.plugins.valheim_serpent_hunter", "Valheim Serpent Hunter", version)]
+  public class BepInExMod : BaseUnityPlugin
+  {
+    public const string version = "0.0.1";
+
+    void Awake()
+    {
+      BepInEx.Logging.Logger.Listeners.Add(new SerpentLogListner());
+
+      var harmony = new Harmony("mod.valheim_serpent_hunter");
+      harmony.PatchAll();
+    }
+  }
+}
---
+++ src/SerpentLogListner.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ValheimSherpentHunter
+{
+  internal class SerpentLogListner : ILogListener
+  {
+    public void Dispose()
+    {
+    }
+
+    public void LogEvent(object sender, LogEventArgs eventArgs)
+    {
+      var message = eventArgs.ToString();
+      if (message.Contains("Spawned Serpent"))
+      {
+        MessageHud.instance.ShowBiomeFoundMsg("Serpent Spawned", true);
+      }
+    }
+  }
+}
